48431ce8f6aa6d0459eee79e7b6df987ac9f3e1d
Gerard Finol <gerard.finol@urv.cat>
2025-12-17 10:46:52 +0100

    update bash script

--- lambda-event-fastq/putObject.sh
+++ lambda-event-fastq/putObject.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Usage: ./putObject.sh <file_path> <s3_bucket> [s3_key]
+# Usage: ./putObject.sh <file_path> [s3_bucket] [s3_key]
+
+DEFAULT_BUCKET="nexus-lambda-events"
 
 startTimeEpochMs=$(date +%s%3N)
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <file_path> <s3_bucket> [s3_key]"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <file_path> [s3_bucket] [s3_key]"
     exit 1
 fi
 
 FILE_PATH="$1"
-S3_BUCKET="$2"
+S3_BUCKET="${2:-$DEFAULT_BUCKET}"
 S3_KEY="${3:-$(basename "$FILE_PATH")}"
-
-# if ! command -v aws &> /dev/null; then
-#     echo "AWS CLI not found. Please install it first."
-#     exit 2
-# fi
 
 echo "Uploading $FILE_PATH to s3://$S3_BUCKET/$S3_KEY ..."
 
@@ -32,10 +29,7 @@ else
     exit 3
 fi
 
-
 echo "Elapsed time (ms): $elapsedTimeMs"
-
-
 
 json_output="{\"startTimeMs\": $startTimeEpochMs, \"endTimeMs\": $endTimeEpochMs, \"elapsedTimeMs\": $elapsedTimeMs}"
 timestamp=$(date +"%Y%m%d_%H%M%S")
